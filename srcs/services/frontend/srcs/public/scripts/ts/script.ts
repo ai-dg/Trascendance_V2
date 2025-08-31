32203bf02f7a94eeb9d29d6064fff51193977f3d
harmoos <harstn@tutanota.com>
2025-08-31 17:57:39 +0200

    Just after merge

--- srcs/services/frontend/srcs/public/scripts/ts/script.ts
+++ srcs/services/frontend/srcs/public/scripts/ts/script.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-import type { Translations } from './types'
-
-console.log("Script working properly");  // to remove
-
-=======
 import test from 'node:test';
 import { getDisconnectedHome, getConnectedHome, initConnectedHome, initDisconnectedHome, getSignupForm} from './interface.js';
 import { initCSRFToken, isConnectedUser, logUser, registerUser } from './login.js';
@@ -14,55 +8,19 @@ import { getUrl } from './urls.js';
 console.log("Script working properly");  // to remove
 
 
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
 document.addEventListener("DOMContentLoaded", () => {
   loadLanguage(languages[currentLangIndex].code, 'home');
 });
 
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-function showSignUp(text: Translations) {
-=======
 
 export function showSignUp(text: Translations) {
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   console.log(">> showSignUp() called"); // to remove
   const contentDiv = document.getElementById('content') as HTMLDivElement;
   if (!contentDiv)
     console.error("Failed to find content element");
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-  contentDiv.innerHTML = `
-    <h2 class="text-xl font-bold mb-6 text-white">${text.signupTitle}</h2>
-    <form class="flex flex-col space-y-4">
-      <input id="login" type="text" required placeholder="${text.login}" class="px-4 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none">
-      <input id="email" type="text" required placeholder="${text.email}" class="px-4 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none">
-      <div class="relative">
-        <input id="passwd" type="password" required placeholder="${text.passwd}" class="px-4 py-2 pr-10 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none w-full">
-        <button type="button" id="togglePasswd" required class="absolute right-2 top-1/2 transform -translate-y-1/2 text-sm text-gray-300 hover:text-white">
-          👁️
-        </button>
-      </div>
-      <div class="relative">
-        <input id="passwdConfirm" type="password" placeholder="${text.passwdConfirm}" class="px-4 py-2 pr-10 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none w-full">
-        <button type="button" id="togglePasswdConfirm" class="absolute right-2 top-1/2 transform -translate-y-1/2 text-sm text-gray-300 hover:text-white">
-          👁️
-        </button>
-      </div>
-      <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded">${text.signupBtn}</button>
-      <div id="formErrors" class="text-red-500 text-sm italic mt-2"></div>
-      </form>
-    <br>
-    <h3 class="text-xl font-bold mb-6 text-white">${text.otherUp}</h3>
-    <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded">GOOGLE SIGN IN</button>
-    <br><br>
-    <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded">42AUTH</button>
-    <br>
-    <button id="backBtn" class="mt-4 text-blue-400 underline">${text.back}</button>
-  `;
-=======
 
   // update DOM
   contentDiv.innerHTML = getSignupForm(text);
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
 
   // show password button
   const togglePasswdBtn = document.getElementById('togglePasswd') as HTMLButtonElement;
@@ -155,14 +113,10 @@ export function showSignUp(text: Translations) {
     errorDiv.innerHTML = '';
 
     // To remove after auth working
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-    showVerificationCode(text);
-=======
     const view = 'signup';
 	
 	registerUser(login, passwd, email, text, view);
     //showVerificationCode(text, view);
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
 
   });
 
@@ -171,18 +125,11 @@ export function showSignUp(text: Translations) {
   if (!backBtn)
     console.error("Failed to find backBtn element");
   backBtn.addEventListener("click", () => showHome(text));
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-}
-
-// show verification code for 2FA
-function showVerificationCode(text: Translations) {
-=======
 
 }
 
 // show verification code for 2FA
 export function showVerificationCode(text: Translations, view: string, params: params) {
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   const contentDiv = document.getElementById('content') as HTMLDivElement;
   if (!contentDiv) {
     console.error("Failed to find content element");
@@ -190,11 +137,7 @@ export function showVerificationCode(text: Translations, view: string, params: p
   }
 
   contentDiv.innerHTML = `
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-    <h2 class="text-xl font-bold mb-4 text-white">${text.verifyTitle}</h2>
-=======
     <h2 class="otp-check text-xl font-bold mb-4 text-white">${text.verifyTitle}</h2>
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
     <p class="text-white mb-4">${text.verifyInstruction}</p>
     <div id="codeContainer" class="flex justify-center space-x-2">
       ${Array.from({ length: 6 })
@@ -222,24 +165,12 @@ export function showVerificationCode(text: Translations, view: string, params: p
   const verifyBtn = document.getElementById('verifyBtn') as HTMLButtonElement;
   if (!verifyBtn)
     console.error("Failed to find verifyBtn element");
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-  verifyBtn.addEventListener('click', () => {
-    const code = Array.from(inputs).map(i => i.value).join('');
-  });
-=======
   verifyBtn.addEventListener('click', async () => OTPValidationHandler(params, inputs));
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
 
   // back button
   const backBtn = document.getElementById("backBtn") as HTMLButtonElement;
   if (!backBtn)
     console.error("Failed to find backBtn element");
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-  backBtn.addEventListener("click", () => showSignIn(text));
-}
-
-function showSignIn(text: Translations) {
-=======
   if (view === 'signin')
     backBtn.addEventListener("click", () => showSignIn(text));
   else if (view === 'signup')
@@ -247,7 +178,6 @@ function showSignIn(text: Translations) {
 }
 
 export function showSignIn(text: Translations) {
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   console.log(">> showSignIn() called");  // to remove
   const contentDiv = document.getElementById('content') as HTMLDivElement;
   if (!contentDiv)
@@ -255,25 +185,16 @@ export function showSignIn(text: Translations) {
   contentDiv.innerHTML = `
     <h2 class="text-xl font-bold mb-6 text-white">${text.signinTitle}</h2>
     <form class="flex flex-col space-y-4">
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-      <input type="text" required placeholder="${text.login}" class="px-4 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none">
-      <div class="relative">
-        <input type="password" required placeholder="${text.passwd}" class="px-4 py-2 pr-10 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none w-full">
-=======
       <input type="text" name="pseudo" required placeholder="${text.login}" class="px-4 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none">
       <div class="relative">
         <input type="password" name"password" required placeholder="${text.passwd}" class="px-4 py-2 pr-10 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none w-full">
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
         <button type="button" id="togglePasswd" class="absolute right-2 top-1/2 transform -translate-y-1/2 text-sm text-gray-300 hover:text-white">
           👁️
         </button>
       </div>
       <button id="forgotPasswd" type="submit" class="text-sm italic bg-transparent text-red-600 border-none hover:underline">${text.forgotPasswd}</button>
       <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded">${text.signin}</button>
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-=======
 	  <div id="formErrors" class="text-red-500 text-sm italic mt-2"></div>
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
     </form>
     <br>
     <h3 class="text-xl font-bold mb-6 text-white">${text.other}</h3>
@@ -300,15 +221,12 @@ export function showSignIn(text: Translations) {
     });
   }
 
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-=======
 
    const errorDiv = document.getElementById('formErrors') as HTMLDivElement;
     if (!errorDiv)
       console.error("Failed to find errorDiv element");
 
 
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   // login form
   const form = document.querySelector("form") as HTMLFormElement;
   if (!form)
@@ -323,16 +241,10 @@ export function showSignIn(text: Translations) {
     if (!passwdInput)
       console.error("Failed to find input element");
     const passwd = passwdInput.value;
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-
-    // To remove after auth working
-    showVerificationCode(text);
-=======
     // To remove after auth working
     const view = 'signin';
 	logUser(login, passwd, text, view);
     // showVerificationCode(text, view);
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   });
 
   // forgot password button
@@ -348,11 +260,7 @@ export function showSignIn(text: Translations) {
   backBtn.addEventListener("click", () => showHome(text));
 }
 
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-function showForgotPasswd(text: Translations) {
-=======
 export function showForgotPasswd(text: Translations) {
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   console.log(">> showForgotPasswd() called");  // to remove
   const contentDiv = document.getElementById('content') as HTMLDivElement;
   if (!contentDiv)
@@ -360,18 +268,12 @@ export function showForgotPasswd(text: Translations) {
   contentDiv.innerHTML = `
     <h2 class="text-xl font-bold mb-6 text-white">${text.forgotPasswd}</h2>
     <form class="flex flex-col space-y-4">
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-      <input type="text" placeholder="${text.email}" class="px-4 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none">
-=======
       <input type="text" name="email" placeholder="${text.email}" class="px-4 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none">
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
       <button type="forgotPasswd" class="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded">${text.resetPasswd}</button>
       <button id="backBtn" class="mt-4 text-blue-400 underline">${text.back}</button>
       </form>
   `;
 
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-=======
   const form = document.querySelector("form") as HTMLFormElement;
   if (!form)
     console.error("Failed to find form element");
@@ -414,7 +316,6 @@ export function showForgotPasswd(text: Translations) {
 
 })
 
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   // back button
   const backBtn = document.getElementById("backBtn") as HTMLButtonElement;
   if (!backBtn)
@@ -422,11 +323,7 @@ export function showForgotPasswd(text: Translations) {
   backBtn.addEventListener("click", () => showSignIn(text));
 }
 
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-function showGuestPlay(text: Translations) {
-=======
 export function showGuestPlay(text: Translations) {
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   console.log(">> showGuestPlay() called");  // to remove
   const contentDiv = document.getElementById('content') as HTMLDivElement;
   if (!contentDiv)
@@ -481,11 +378,7 @@ export function showGuestPlay(text: Translations) {
   backBtn.addEventListener("click", () => showHome(text));
 }
 
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-function showOptions(text: Translations) {
-=======
 export function showOptions(text: Translations) {
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   console.log(">> showOptions() called");  // to remove
   const contentDiv = document.getElementById('content') as HTMLDivElement;
   if (!contentDiv)
@@ -512,59 +405,16 @@ export function showOptions(text: Translations) {
   const backBtn = document.getElementById("backBtn") as HTMLButtonElement;
   if (!backBtn)
     console.error("Failed to find backBtn element");
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-  backBtn.addEventListener("click", () => showHome(text));}
-
-function showHome(text: Translations) {
-=======
   backBtn.addEventListener("click", async () => showHome(text));
 
 }
 
 export async function showHome(text: Translations) {
 	
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   if (!text) return;
   const contentDiv = document.getElementById('content') as HTMLDivElement;
   if (!contentDiv)
     console.error("Failed to find content element");
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-  contentDiv.innerHTML = `
-    <h1 id="title" class="text-2xl font-bold text-white mb-6">${text.title}</h1>
-    <div class="flex flex-col space-y-4" id="buttonsContainer">
-      <button id="signupBtn" class="bg-transparent text-red-600 border-none hover:underline">${text.signup}</button>
-      <button id="signinBtn" class="bg-transparent text-white border-none hover:underline">${text.signin}</button>
-      <button id="playAsGuestBtn" class="bg-transparent text-white border-none hover:underline">${text.playAsGuest}</button>
-      <button id="optionsBtn" class="bg-transparent text-white border-none hover:underline">${text.options}</button>
-      <button id="aboutBtn" class="bg-transparent text-white border-none hover:underline">${text.about}</button>
-    </div>
-  `;
-
-  // sign up button
-  const signupBtn = document.getElementById("signupBtn") as HTMLButtonElement;
-  if (!signupBtn)
-    console.error("Failed to find signupBtn element");
-  signupBtn.addEventListener("click", () => showSignUp(text));
-
-  // sign in button
-  const signinBtn = document.getElementById("signinBtn") as HTMLButtonElement;
-  if (!signinBtn)
-    console.error("Failed to find signinBtn element");
-  signinBtn.addEventListener("click", () => showSignIn(text));
-
-  // play as guest button
-  const playAsGuestBtn = document.getElementById("playAsGuestBtn") as HTMLButtonElement;
-  if (!playAsGuestBtn)
-    console.error("Failed to find playAsGuestBtn element");
-  playAsGuestBtn.addEventListener("click", () => showGuestPlay(text));
-
-  // options button
-  const optionsBtn = document.getElementById("optionsBtn") as HTMLButtonElement;
-  if (!optionsBtn)
-    console.error("Failed to find optionsBtn element");
-  optionsBtn.addEventListener("click", () => showOptions(text));
-
-=======
 
 	const isConnected = await isConnectedUser();
 console.log("user is connected : ", isConnected);
@@ -579,7 +429,6 @@ console.log("user is connected : ", isConnected);
 		contentDiv.innerHTML = await getDisconnectedHome();
 		await initDisconnectedHome(text);
 	}
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
 }
 
 const languages = [
@@ -609,11 +458,6 @@ async function loadLanguage(langCode: string, view = 'home') {
   }
 }
 
-<<<<<<< HEAD:srcs/requirements/server-rendering/app/srcs/public/scripts/ts/script.ts
-function toggleLanguage() {
-  currentLangIndex = (currentLangIndex + 1) % languages.length;
-  const nextLang = languages[currentLangIndex].code;
-=======
 export async function getTraductions(){
 	const html = document.querySelector("html")
   	const langCode =html?.getAttribute("lang")
@@ -641,7 +485,6 @@ function toggleLanguage() {
   const nextLang = languages[currentLangIndex].code;
   const html = document.querySelector("html")
   html?.setAttribute("lang", nextLang)
->>>>>>> main:srcs/services/frontend/srcs/public/scripts/ts/script.ts
   loadLanguage(nextLang, 'options');
 }
 
